6eaa394b426090cbe59f1e2f17ab730c8292c930
aliaga@uji.es <aliaga@tintorrum.local>
2020-04-29 16:43:28 +0200

    Inclusion of the cuda executor in the repository:     * For CGS, a loop of kernels is used instead of a kernel with a loop.     * The test files are not repaired yet.

--- include/ginkgo/core/base/types.hpp
+++ include/ginkgo/core/base/types.hpp
@@ -454,6 +454,17 @@ GKO_ATTRIBUTES constexpr bool operator!=(precision_reduction x,
  *                Should take one argument, which is replaced by the
  *                value type.
  */
+#define GKO_INSTANTIATE_FOR_SIZE_TYPE(_macro) template _macro(size_type)
+
+
+/**
+ * Instantiates a template for each index type compiled by Ginkgo.
+ *
+ * @param _macro  A macro which expands the template instantiation
+ *                (not including the leading `template` specifier).
+ *                Should take one argument, which is replaced by the
+ *                value type.
+ */
 #define GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(_macro) \
     template _macro(int32);                         \
     template _macro(int64)
