4cf7991aa325213b83fc6eb108e8e0c34936c9bf
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2020-05-25 11:02:05 +0200

    Change const accessor to non-const in check_arnoldi_norms_new
    This allows it to compile with the writing of the scale.

--- cuda/solver/gmres_mixed_kernels.cu
+++ cuda/solver/gmres_mixed_kernels.cu
@@ -739,9 +739,8 @@ void finish_arnoldi_CGS2(
         <<<ceildiv(dim_size[1], default_block_size), default_block_size>>>(
             as_cuda_type(arnoldi_norm->get_values()), stride_arnoldi,
             as_cuda_type(hessenberg_iter->get_values()), stride_hessenberg,
-            iter + 1, as_cuda_accessor(krylov_bases.to_const()),
-            as_cuda_type(stop_status), as_cuda_type(reorth_status),
-            as_cuda_type(num_reorth->get_data()));
+            iter + 1, as_cuda_accessor(krylov_bases), as_cuda_type(stop_status),
+            as_cuda_type(reorth_status), as_cuda_type(num_reorth->get_data()));
     /* */
 #ifdef TIMING
     exec->synchronize();
@@ -866,7 +865,7 @@ void finish_arnoldi_CGS2(
             <<<ceildiv(dim_size[1], default_block_size), default_block_size>>>(
                 as_cuda_type(arnoldi_norm->get_values()), stride_arnoldi,
                 as_cuda_type(hessenberg_iter->get_values()), stride_hessenberg,
-                iter + 1, as_cuda_accessor(krylov_bases.to_const()),
+                iter + 1, as_cuda_accessor(krylov_bases),
                 as_cuda_type(stop_status), as_cuda_type(reorth_status),
                 as_cuda_type(num_reorth->get_data()));
         /* */
