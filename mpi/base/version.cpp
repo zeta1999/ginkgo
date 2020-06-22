e7e2802b44f701b325ade9c1a0c19265832b8207
Pratik Nayak <pratikvn@protonmail.com>
2020-06-22 22:27:24 +0200

    Add version info for MPI module.

--- mpi/base/version.cpp
+++ mpi/base/version.cpp
@@ -36,39 +36,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace gko {
 
 
-version version_info::get_core_version() noexcept
+version version_info::get_mpi_version() noexcept
 {
     // When compiling the module, the header version is the same as the library
     // version. Mismatch between the header and the module versions may happen
     // if using shared libraries from different versions of Ginkgo.
     return version_info::get_header_version();
-}
-
-
-std::ostream &operator<<(std::ostream &os, const version_info &ver_info)
-{
-    auto print_version = [](std::ostream &os, const version &ver) -> void {
-        static const std::string not_compiled_tag = "not compiled";
-        if (ver.tag == not_compiled_tag) {
-            os << "not compiled";
-        } else {
-            os << ver;
-        }
-    };
-
-    os << "This is Ginkgo " << ver_info.header_version
-       << "\n    running with core module " << ver_info.core_version
-       << "\n    the reference module is  ";
-    print_version(os, ver_info.reference_version);
-    os << "\n    the OpenMP    module is  ";
-    print_version(os, ver_info.omp_version);
-    os << "\n    the CUDA      module is  ";
-    print_version(os, ver_info.cuda_version);
-    os << "\n    the MPI module is  ";
-    print_version(os, ver_info.mpi_version);
-    os << "\n    the HIP      module is  ";
-    print_version(os, ver_info.hip_version);
-    return os;
 }
 
 
